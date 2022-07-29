ccb872361afcbb053a84bde61acead850b4f1d03
git-adam <awwojcik1@gmail.com>
2022-07-29 14:38:15 +0200

    final homework solution

--- Homework_3_2/Program.cs
+++ Homework_3_2/Program.cs
@@ -10,6 +10,63 @@ namespace Homework_3_2
     {
         static void Main(string[] args)
         {
+            var lowerLimit = 0;
+            var upperLimit = 100;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.WriteLine($"Spróbuj odgadnąć wylosowaną liczbę, która jest z przedziału [{lowerLimit};{upperLimit}].");
+
+            System.Threading.Thread.Sleep(1000);
+
+            Console.WriteLine("Losujemy liczbę...");
+            var rnd = new Random();
+            var drawnNumber = rnd.Next(lowerLimit, upperLimit);
+            int yourNumber;
+
+            Console.WriteLine(drawnNumber);
+
+            var i = 1;
+            while (true)
+            {
+                Console.Write("Podaj liczbę: ");
+                var flag = false;
+                try
+                {
+                    yourNumber = int.Parse(Console.ReadLine());
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    flag = true;
+                    throw ex;
+                }
+                finally
+                {
+                    if(flag)
+                        Console.WriteLine("Coś się zepsuło!");
+                }
+
+                Console.WriteLine();
+
+                if (yourNumber < drawnNumber && yourNumber >= lowerLimit )
+                {
+                    Console.WriteLine("Podana liczba jest za mała.");
+                }
+                else if (yourNumber > drawnNumber && yourNumber <= upperLimit)
+                {
+                    Console.WriteLine("Podana liczba jest za duża.");
+                }
+                else if (drawnNumber == yourNumber)
+                {
+                    Console.WriteLine($"Brawo! Udało Ci się trafić w {i} próbach.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Podaj liczbę z właściwego zakresu.");
+                }
+
+                i++;
+            }
         }
     }
 }
